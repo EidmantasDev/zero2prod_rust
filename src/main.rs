f93389611bc991f84083f2e6c749d5ab4f8bc128
EidmantasDev <eidmantas.slenys@gmail.com>
2024-02-17 16:26:07 +0200

    Added configuration file, updated tests with psql, connected to db

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 //! main.rs
 use std::net::TcpListener;
+use zero2prod_rust::configuration::get_configuration;
 use zero2prod_rust::startup::run;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
