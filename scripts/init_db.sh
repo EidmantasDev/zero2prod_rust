4b32efc195128b63a0a7f2f1bf73aaba06a339b3
EidmantasDev <eidmantas.slenys@gmail.com>
2024-02-17 14:56:13 +0200

    Updated bash script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -37,12 +37,11 @@ then
         exit 1
     fi
     # Launch postgres using Docker
-    docker run \
+    docker run -d --network=host\
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
-    -d \
     --name "postgres_$(date '+%s')" \
     postgres -N 1000
     # ^ Increased maximum number of connections for testing purposes
