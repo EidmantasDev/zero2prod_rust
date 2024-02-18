be18d45e4febd6aae48dbc37be68f2d1a8910e42
EidmantasDev <eidmantas.slenys@gmail.com>
2024-02-18 09:22:43 +0200

    Init Telemetry Added crates for tracing Updated readme.md with jq info Updated secret not to get exposed in tracing Separted functions from tracing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 //! main.rs
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod_rust::configuration::get_configuration;
 use zero2prod_rust::startup::run;
+use zero2prod_rust::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod_rust".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
